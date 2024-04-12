234c8933851ba2c53fc11a41312c4b7912a8fcfd
Dray <drayttc@gmail.com>
2024-04-11 22:47:38 -0300

    Moves Player class definitions to a separated file

--- include/Player.h
+++ include/Player.h
@@ -16,34 +16,11 @@ class Player {
     Rlib::Rectangle m_hitbox{};
     MYSETTER(const Rlib::Rectangle, hitbox, m_hitbox);
 
-    void correct_position(const Rlib::Rectangle& bounds, const Axis axis) {
-        switch (axis) {
-            using enum Axis;
-        case HORIZONTAL:
-            if (m_hitbox.x < bounds.x)
-                m_hitbox.x = bounds.x;
-            else if (m_hitbox.x + m_hitbox.width > bounds.x + bounds.width)
-                m_hitbox.x = bounds.x + bounds.width - m_hitbox.width;
-            break;
-
-        case VERTICAL:
-            if (m_hitbox.y < bounds.y)
-                m_hitbox.y = bounds.y;
-            else if (m_hitbox.y + m_hitbox.height > bounds.y + bounds.height)
-                m_hitbox.y = bounds.y + bounds.height - m_hitbox.height;
-            break;
-
-        default:
-            break;
-        }
-    };
+    void correct_position(const Rlib::Rectangle& bounds, const Axis axis);
 
   public:
     Player(const Rlib::Rectangle& hitbox, const TrashInfo::Type type,
-           const float speed = 400)
-        : m_hitbox{hitbox}, speed{speed}, m_type{type} {
-        m_color = TrashInfo::colors_map[type];
-    };
+           const float speed = 400);
     Player(Player&&) = default;
     Player(const Player&) = default;
     Player& operator=(Player&&) = default;
@@ -55,82 +32,14 @@ class Player {
 
     MYGETTER(const Rlib::Rectangle, hitbox, m_hitbox);
 
-    void update(const Rlib::Rectangle& bounds) {
-        // Constant height
-        m_hitbox.y = bounds.y + bounds.height - m_hitbox.height - 30;
-
-        this->correct_position(bounds, Axis::HORIZONTAL);
-        this->correct_position(bounds, Axis::VERTICAL);
-    };
+    void update(const Rlib::Rectangle& bounds);
 
     void move(const Direction direction, const Rlib::Rectangle& bounds,
-              const float dt = 0) {
-        switch (direction) {
-            using enum Direction;
-        case UP:
-        case DOWN:
-            break;
-
-        case LEFT:
-            m_hitbox.x -= speed * dt;
-            this->correct_position(bounds, Axis::HORIZONTAL);
-
-            break;
-
-        case RIGHT:
-            m_hitbox.x += speed * dt;
-            this->correct_position(bounds, Axis::HORIZONTAL);
-
-            break;
-
-        default:
-            break;
-        }
-    };
+              const float dt = 0);
 
     void draw() { m_hitbox.Draw(m_color); };
-    void set_type(const TrashInfo::Type type) {
-        m_type = type;
-        m_color = TrashInfo::colors_map[type];
-    };
 
-    void handle_input(const Rlib::Rectangle& bounds, const float dt) {
-        if (IsKeyDown(KEY_A) || IsKeyDown(KEY_LEFT))
-            this->move(Direction::LEFT, bounds, dt);
+    void set_type(const TrashInfo::Type type);
 
-        if (IsKeyDown(KEY_D) || IsKeyDown(KEY_RIGHT))
-            this->move(Direction::RIGHT, bounds, dt);
-
-        int key = GetKeyPressed();
-
-        switch (key) {
-        case KEY_ONE:
-        case KEY_KP_1:
-            this->set_type(TrashInfo::Type::PAPER);
-            break;
-
-        case KEY_TWO:
-        case KEY_KP_2:
-            this->set_type(TrashInfo::Type::GLASS);
-            break;
-
-        case KEY_THREE:
-        case KEY_KP_3:
-            this->set_type(TrashInfo::Type::PLASTIC);
-            break;
-
-        case KEY_FOUR:
-        case KEY_KP_4:
-            this->set_type(TrashInfo::Type::METAL);
-            break;
-
-        case KEY_FIVE:
-        case KEY_KP_5:
-            this->set_type(TrashInfo::Type::ORGANIC);
-            break;
-
-        default:
-            break;
-        }
-    };
+    void handle_input(const Rlib::Rectangle& bounds, const float dt);
 };
---
+++ src/Player.cpp
@@ -0,0 +1,110 @@
+#include "Player.h"
+
+/* ---------- Private methods ---------- */
+void Player::correct_position(const Rlib::Rectangle& bounds, const Axis axis) {
+    switch (axis) {
+        using enum Axis;
+    case HORIZONTAL:
+        if (m_hitbox.x < bounds.x)
+            m_hitbox.x = bounds.x;
+        else if (m_hitbox.x + m_hitbox.width > bounds.x + bounds.width)
+            m_hitbox.x = bounds.x + bounds.width - m_hitbox.width;
+        break;
+
+    case VERTICAL:
+        if (m_hitbox.y < bounds.y)
+            m_hitbox.y = bounds.y;
+        else if (m_hitbox.y + m_hitbox.height > bounds.y + bounds.height)
+            m_hitbox.y = bounds.y + bounds.height - m_hitbox.height;
+        break;
+
+    default:
+        break;
+    }
+};
+
+/* ---------- Public methods ---------- */
+
+Player::Player(const Rlib::Rectangle& hitbox, const TrashInfo::Type type,
+               const float speed)
+    : m_hitbox{hitbox}, speed{speed}, m_type{type} {
+    m_color = TrashInfo::colors_map[type];
+};
+
+void Player::update(const Rlib::Rectangle& bounds) {
+    // Constant height
+    m_hitbox.y = bounds.y + bounds.height - m_hitbox.height - 30;
+
+    this->correct_position(bounds, Axis::HORIZONTAL);
+    this->correct_position(bounds, Axis::VERTICAL);
+};
+
+void Player::move(const Direction direction, const Rlib::Rectangle& bounds,
+                  const float dt) {
+    switch (direction) {
+        using enum Direction;
+    case UP:
+    case DOWN:
+        break;
+
+    case LEFT:
+        m_hitbox.x -= speed * dt;
+        this->correct_position(bounds, Axis::HORIZONTAL);
+
+        break;
+
+    case RIGHT:
+        m_hitbox.x += speed * dt;
+        this->correct_position(bounds, Axis::HORIZONTAL);
+
+        break;
+
+    default:
+        break;
+    }
+};
+
+void Player::set_type(const TrashInfo::Type type) {
+    m_type = type;
+    m_color = TrashInfo::colors_map[type];
+};
+
+void Player::handle_input(const Rlib::Rectangle& bounds, const float dt) {
+    if (IsKeyDown(KEY_A) || IsKeyDown(KEY_LEFT))
+        this->move(Direction::LEFT, bounds, dt);
+
+    if (IsKeyDown(KEY_D) || IsKeyDown(KEY_RIGHT))
+        this->move(Direction::RIGHT, bounds, dt);
+
+    int key = GetKeyPressed();
+
+    switch (key) {
+    case KEY_ONE:
+    case KEY_KP_1:
+        this->set_type(TrashInfo::Type::PAPER);
+        break;
+
+    case KEY_TWO:
+    case KEY_KP_2:
+        this->set_type(TrashInfo::Type::GLASS);
+        break;
+
+    case KEY_THREE:
+    case KEY_KP_3:
+        this->set_type(TrashInfo::Type::PLASTIC);
+        break;
+
+    case KEY_FOUR:
+    case KEY_KP_4:
+        this->set_type(TrashInfo::Type::METAL);
+        break;
+
+    case KEY_FIVE:
+    case KEY_KP_5:
+        this->set_type(TrashInfo::Type::ORGANIC);
+        break;
+
+    default:
+        break;
+    }
+};
